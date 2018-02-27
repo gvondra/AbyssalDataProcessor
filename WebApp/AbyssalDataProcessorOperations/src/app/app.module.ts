8b170342317081613a3f7d8c2f2d4f748ada01c9
Greg Vondra <gvondra@gmail.com>
2018-02-27 17:33:34 -0600

    Copy from JCU web app

---
+++ WebApp/AbyssalDataProcessorOperations/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router'
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { RequestMembershipComponent } from './request-membership/request-membership.component';
+import { MenuComponent } from './menu/menu.component';
+import { ConfigurationService } from './configuration.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth/auth.service';
+import { CallbackComponent } from './callback/callback.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    RequestMembershipComponent,
+    MenuComponent,
+    CallbackComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [ConfigurationService, AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
