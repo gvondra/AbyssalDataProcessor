16717590b6a1fb0d4194fa11c2f8dcbbf2ba4ce9
Greg Vondra <gvondra@gmail.com>
2018-03-03 08:11:57 -0600

    Update to mock menu items

--- WebApp/AbyssalDataProcessorOperations/src/app/mock-menu-items.ts
+++ WebApp/AbyssalDataProcessorOperations/src/app/mock-menu-items.ts
@@ -2,6 +2,13 @@ import { MenuItem } from './menu-item';
 
 export const MENU_ITEMS: Array<Array<MenuItem>> = [
     [
-        {Text: "Home", URL: ""}
+        {Text: "Home", URL: ""},
+        {Text: "Organizations", URL: ""}
+    ],
+    [
+        {Text: "Tasks", URL: ""},
+        {Text: "Users", URL: ""},
+        {Text: "Metrics", URL: ""},
+        {Text: "Exceptions", URL: ""}
     ]
 ]
