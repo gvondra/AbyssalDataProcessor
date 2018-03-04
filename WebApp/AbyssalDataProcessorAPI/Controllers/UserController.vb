f4bd81d32fc390fa508bf179883f898d0d093230
Greg Vondra <gvondra@gmail.com>
2018-03-04 10:44:12 -0600

    Add api project

---
+++ WebApp/AbyssalDataProcessorAPI/Controllers/UserController.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Net
+Imports System.Security.Claims
+Imports System.Web.Http
+
+Namespace Controllers
+    Public Class UserController
+        Inherits ApiController
+
+        <HttpGet(), Authorize()> Public Function GetUser() As IHttpActionResult
+            Dim user As ClaimsPrincipal = CType(Me.User, ClaimsPrincipal)
+            Dim claim As Claim = user.Claims.FirstOrDefault(Function(c As Claim) c.Type = ClaimTypes.NameIdentifier)
+
+            If claim IsNot Nothing Then
+
+            End If
+
+            Return Ok()
+        End Function
+
+    End Class
+End Namespace
