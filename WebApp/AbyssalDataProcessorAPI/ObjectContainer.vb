0d0e487ed4d8f1a9d2fbdf9b4f6ebf5b44d36a0f
Greg Vondra <gvondra@gmail.com>
2018-03-04 12:28:02 -0600

    User controller update

---
+++ WebApp/AbyssalDataProcessorAPI/ObjectContainer.vb
@@ -0,0 +1,18 @@
+﻿Imports AbyssalDataProcessor.Core.User
+Imports Autofac
+Friend NotInheritable Class ObjectContainer
+
+    Private Shared m_container As IContainer
+
+    Shared Sub New()
+        Dim builder As New ContainerBuilder()
+
+        builder.RegisterType(Of UserFactory).As(Of IUserFactory)()
+
+        m_container = builder.Build()
+    End Sub
+
+    Public Shared Function GetContainer() As IContainer
+        Return m_container
+    End Function
+End Class
---
+++ WebApp/AbyssalDataProcessorAPI/Settings.vb
@@ -0,0 +1,9 @@
+﻿Public Class Settings
+    Implements AbyssalDataProcessor.Core.Framework.ISettings
+
+    Public ReadOnly Property ConnectionString As String Implements ISettings.ConnectionString
+        Get
+            Return ConfigurationManager.ConnectionStrings("adp").ConnectionString
+        End Get
+    End Property
+End Class
