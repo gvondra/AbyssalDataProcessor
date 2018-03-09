c2730ea18e7b8135b858680ffc6cf030e1a500f1
Greg Vondra <gvondra@gmail.com>
2018-03-08 21:35:20 -0600

    Add saver, creater, and updater objects

---
+++ Core/Core.Framework/ISavable.vb
@@ -0,0 +1,4 @@
+﻿Public Interface ISavable
+    Function GetDataCreator(ByVal settings As ISettings) As IDataCreator
+    Function GetDataUpdater(ByVal settings As ISettings) As IDataUpdater
+End Interface
---
+++ Core/Core.Utilities/Saver.vb
@@ -0,0 +1,25 @@
+﻿Public Class Saver
+    Public Sub Save(ByVal settings As Framework.ISettings, ByVal save As Action)
+        Try
+            save.Invoke()
+
+            If settings.DbTransaction IsNot Nothing Then
+                settings.DbTransaction.Commit()
+            End If
+        Catch
+            If settings.DbTransaction IsNot Nothing Then
+                settings.DbTransaction.Rollback()
+            End If
+            Throw
+        Finally
+            If settings.DbTransaction IsNot Nothing Then
+                settings.DbTransaction.Dispose()
+                settings.DbTransaction = Nothing
+            End If
+            If settings.DbConnection IsNot Nothing Then
+                settings.DbConnection.Dispose()
+                settings.DbConnection = Nothing
+            End If
+        End Try
+    End Sub
+End Class
