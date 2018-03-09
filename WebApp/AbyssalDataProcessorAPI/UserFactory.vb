3548410c83f0f96c6d0b429a302f983574bf23b1
Greg Vondra <gvondra@gmail.com>
2018-03-08 22:07:45 -0600

    Update to load and create users

--- WebApp/AbyssalDataProcessorAPI/UserFactory.vb
+++ WebApp/AbyssalDataProcessorAPI/UserFactory.vb
@@ -23,15 +23,66 @@ Public Class UserFactory
     Public Function [Get](principal As ClaimsPrincipal) As IUser Implements IUserFactory.Get
         Dim id As Claim = principal.Claims.FirstOrDefault(Function(c As Claim) c.Type = ClaimTypes.NameIdentifier)
         Dim user As IUser = Nothing
+        Dim claim As Claim = Nothing
 
         If id IsNot Nothing Then
             user = GetBySubscriberId(New Settings(), id.Value)
         End If
 
+        If user Is Nothing Then
+            claim = principal.Claims.FirstOrDefault(Function(c As Claim) c.Type = ClaimTypes.Email)
+            If claim IsNot Nothing Then
+                user = GetByEmailAddress(New Settings(), claim.Value)
+                UpdateUser(principal, user, id)
+            End If
+        End If
+
+        If user Is Nothing Then
+            user = Create()
+            UpdateUser(principal, user, id)
+        End If
+
         Return user
     End Function
 
+    Private Sub UpdateUser(ByVal principal As ClaimsPrincipal, ByVal user As IUser, ByVal id As Claim)
+        Dim claim As Claim = Nothing
+
+        claim = principal.Claims.FirstOrDefault(Function(c As Claim) c.Type = ClaimTypes.Email)
+        If claim IsNot Nothing AndAlso String.IsNullOrEmpty(claim.Value) = False AndAlso String.IsNullOrEmpty(user.EmailAddress) Then
+            user.EmailAddress = claim.Value
+        End If
+
+        claim = principal.Claims.FirstOrDefault(Function(c As Claim) c.Type = ClaimTypes.Name)
+        If claim IsNot Nothing AndAlso String.IsNullOrEmpty(claim.Value) = False Then
+            If String.IsNullOrEmpty(user.FullName) Then
+                user.FullName = claim.Value
+            End If
+            If String.IsNullOrEmpty(user.ShortName) Then
+                user.ShortName = claim.Value
+            End If
+        End If
+
+        claim = principal.Claims.FirstOrDefault(Function(c As Claim) c.Type = ClaimTypes.DateOfBirth)
+        If claim IsNot Nothing AndAlso String.IsNullOrEmpty(claim.Value) = False AndAlso user.BirthDate.HasValue = False Then
+            user.BirthDate = Date.Parse(claim.Value).Date
+        End If
+
+        Using scope = m_container.BeginLifetimeScope
+            Dim saver As IUserSaver = scope.Resolve(Of IUserSaver)
+            saver.Save(New Settings(), user, id.Value)
+        End Using
+    End Sub
+
     Public Function GetBySubscriberId(settings As ISettings, subscriberId As String) As IUser Implements AbyssalDataProcessor.Core.Framework.IUserFactory.GetBySubscriberId
         Return m_innerUserFactory.GetBySubscriberId(settings, subscriberId)
+    End Function
+
+    Public Function Create() As IUser Implements AbyssalDataProcessor.Core.Framework.IUserFactory.Create
+        Return m_innerUserFactory.Create()
+    End Function
+
+    Public Function GetByEmailAddress(settings As ISettings, emailAddress As String) As IUser Implements AbyssalDataProcessor.Core.Framework.IUserFactory.GetByEmailAddress
+        Return m_innerUserFactory.GetByEmailAddress(settings, emailAddress)
     End Function
 End Class
