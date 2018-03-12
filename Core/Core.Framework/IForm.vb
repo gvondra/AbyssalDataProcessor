5eaa15448697fb317dc7f56b6b7d905c063e46e0
Greg Vondra <gvondra@gmail.com>
2018-03-12 18:53:36 -0500

    Add form interfaces

---
+++ Core/Core.Framework/IForm.vb
@@ -0,0 +1,7 @@
+﻿Imports System.Xml
+Public Interface IForm
+    ReadOnly Property FormId As Guid
+    ReadOnly Property FormTypeId As enumFormType
+    Property Style As enumFormStyle
+    ReadOnly Property Content As XmlNode
+End Interface
---
+++ Core/Core.Framework/IFormFactory.vb
@@ -0,0 +1,5 @@
+﻿Imports System.Xml
+Public Interface IFormFactory
+    Function CreateRoleRequest() As Forms.IRoleRequest
+    Function CreateForm(ByVal user As IUser, ByVal formType As enumFormType, ByVal style As enumFormStyle, ByVal content As XmlNode) As IForm
+End Interface
