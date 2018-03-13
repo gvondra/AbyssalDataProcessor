d8b590361ff9d5c278c2538ef172b089c8fd31df
Greg Vondra <gvondra@gmail.com>
2018-03-12 21:58:20 -0500

    Add logging project

---
+++ Core/Core.Logging/ObjectContainer.vb
@@ -0,0 +1,19 @@
+﻿Imports AbyssalDataProcessor.DataTier.Core
+Imports Autofac
+Friend NotInheritable Class ObjectContainer
+
+    Private Shared m_container As IContainer
+
+    Shared Sub New()
+        Dim builder As New ContainerBuilder()
+
+        builder.RegisterType(Of WebMetricDataSaver)()
+        builder.RegisterType(Of WebMetricAttributeDataSaver)()
+
+        m_container = builder.Build()
+    End Sub
+
+    Public Shared Function GetContainer() As IContainer
+        Return m_container
+    End Function
+End Class
