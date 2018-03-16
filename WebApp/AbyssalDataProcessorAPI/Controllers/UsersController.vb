13e85eb89684840c8d83954da1ec382c13c81a67
Greg Vondra <gvondra@gmail.com>
2018-03-16 11:30:41 -0500

    Add users controller with search method

---
+++ WebApp/AbyssalDataProcessorAPI/Controllers/UsersController.vb
@@ -0,0 +1,72 @@
+﻿Imports Autofac
+Imports AutoMapper
+Imports System.Text.RegularExpressions
+Imports System.Net
+Imports System.Web.Http
+
+Namespace Controllers
+    <MetricsLog()>
+    Public Class UsersController
+        Inherits ControllerBase
+
+        Private Shared m_mapperConfiguration As MapperConfiguration
+
+        Shared Sub New()
+            m_mapperConfiguration = New MapperConfiguration(Sub(c As IMapperConfigurationExpression)
+                                                                c.CreateMap(Of IUser, User)()
+                                                            End Sub)
+        End Sub
+
+        <HttpGet(), ClaimsAuthorization(ClaimTypes:="UA"), Route("api/Users/Search")>
+        Function Search(ByVal s As String) As IHttpActionResult
+            Dim result As IHttpActionResult = Nothing
+            Dim userId As Guid?
+            Dim userFactory As IUserFactory
+            Dim innerUsers As IEnumerable(Of IUser) = Nothing
+            Dim u As IUser
+            Dim users As IEnumerable(Of User)
+            Dim mapper As IMapper
+
+            If String.IsNullOrEmpty(s) Then
+                result = BadRequest("Missing search text")
+            End If
+
+            If result Is Nothing Then
+                Using scope As ILifetimeScope = Me.ObjectContainer.BeginLifetimeScope
+                    userFactory = scope.Resolve(Of IUserFactory)()
+                    userId = SearchToGuid(s)
+                    If userId.HasValue Then
+                        u = userFactory.Get(New Settings(), userId.Value)
+                        If u IsNot Nothing Then
+                            innerUsers = {u}
+                        Else
+                            innerUsers = New List(Of IUser)()
+                        End If
+                    Else
+                        innerUsers = userFactory.Search(New Settings(), s)
+                    End If
+                End Using
+            End If
+
+            If result Is Nothing Then
+                mapper = New Mapper(m_mapperConfiguration)
+                users = From y In innerUsers.Select(Of User)(Function(x As IUser) mapper.Map(Of User)(x))
+                result = Ok(users)
+            End If
+
+            Return result
+        End Function
+
+        Private Function SearchToGuid(ByVal s As String) As Guid?
+            Dim g As Guid? = Nothing
+            Dim h As Guid
+            s = Regex.Replace(s, "[^0-9A-Fa-f]+", String.Empty)
+            If Regex.IsMatch(s, "[0-9A-Fa-f]{32}") Then
+                If Guid.TryParse(s, h) Then
+                    g = h
+                End If
+            End If
+            Return g
+        End Function
+    End Class
+End Namespace
