6a1cceb84657afdf238912d20a39130c41a3141f
Greg Vondra <gvondra@gmail.com>
2018-03-21 11:05:17 -0500

    Add get task api method

---
+++ WebApp/AbyssalDataProcessorAPI/Controllers/TaskController.vb
@@ -0,0 +1,43 @@
+﻿Imports Autofac
+Imports AutoMapper
+Imports System.Net
+Imports System.Web.Http
+
+Namespace Controllers
+    <MetricsLog()>
+    Public Class TaskController
+        Inherits ControllerBase
+
+        <HttpGet(), ClaimsAuthorization(ClaimTypes:="TP|TA")> Public Function GetTask(ByVal id As Guid) As IHttpActionResult
+            Dim result As IHttpActionResult = Nothing
+            Dim mapper As IMapper
+            Dim mapperConfiguration As TaskMapper
+            Dim taskFactory As ITaskFactory
+            Dim task As Task
+            Dim innerTask As ITask = Nothing
+
+            If id.Equals(Guid.Empty) Then
+                result = BadRequest("Missing or invalid task id")
+            End If
+
+            Using scope As ILifetimeScope = Me.ObjectContainer.BeginLifetimeScope
+                If result Is Nothing Then
+                    taskFactory = scope.Resolve(Of ITaskFactory)()
+                    innerTask = taskFactory.Get(New Settings(), id)
+                    If innerTask Is Nothing Then
+                        result = NotFound()
+                    End If
+                End If
+
+                If result Is Nothing Then
+                    mapperConfiguration = New TaskMapper
+                    mapper = New Mapper(mapperConfiguration.MapperConfiguration)
+                    task = mapper.Map(Of Task)(innerTask)
+                    result = Ok(task)
+                End If
+            End Using
+
+            Return result
+        End Function
+    End Class
+End Namespace
