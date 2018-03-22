2131b50b518e391e55ccba87d599152e918f6a85
Greg Vondra <gvondra@gmail.com>
2018-03-22 11:01:00 -0500

    Display task properties

--- WebApp/AbyssalDataProcessorOperations/src/app/task/task.component.ts
+++ WebApp/AbyssalDataProcessorOperations/src/app/task/task.component.ts
@@ -1,15 +1,39 @@
+import 'rxjs/add/operator/switchMap';
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Task } from '../task';
+import { TaskService } from '../task.service';
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css']
+  styleUrls: ['./task.component.css'],
+  providers: [ TaskService ]
 })
 export class TaskComponent implements OnInit {
 
-  constructor() { }
+  Task: Task = null;
+  SpinnerHidden: boolean = true;
+
+  constructor(private route: ActivatedRoute, private taskService: TaskService) { }
+
+  FormatTimestamp(ts): string {
+    if (ts && Date.parse(ts)) {
+      let d = new Date(Date.parse(ts));
+      ts = d.toLocaleString();
+    }
+    return ts
+  }
 
   ngOnInit() {
+    this.route.params
+    .switchMap((params: ParamMap) => {
+      this.SpinnerHidden = false;
+      return this.taskService.getTask(params['id']);
+    })
+    .subscribe(task => {
+      this.Task = task;
+      this.SpinnerHidden = true;
+    });
   }
 
 }
