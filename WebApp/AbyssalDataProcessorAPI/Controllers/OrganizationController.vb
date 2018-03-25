cf6dd783290eff31c6c4302565509dabaa07c337
Greg Vondra <gvondra@gmail.com>
2018-03-25 07:53:11 -0500

    Add get organization api method

--- WebApp/AbyssalDataProcessorAPI/Controllers/OrganizationController.vb
+++ WebApp/AbyssalDataProcessorAPI/Controllers/OrganizationController.vb
@@ -17,24 +17,52 @@ Namespace Controllers
                                                             End Sub)
         End Sub
 
+        'todo add error handling
+        <HttpGet(), ClaimsAuthorization(ClaimTypes:="OA")> Public Shadows Function GetOrganization(ByVal id As Guid) As IHttpActionResult
+            Dim result As IHttpActionResult = Nothing
+            Dim innerOrganization As IOrganization
+            Dim organizationFactory As IOrganizationFactory
+            Dim mapper As IMapper
+            Using scope As ILifetimeScope = Me.ObjectContainer.BeginLifetimeScope
+                organizationFactory = scope.Resolve(Of IOrganizationFactory)()
+                innerOrganization = organizationFactory.Get(New Settings(), id)
+
+                If innerOrganization Is Nothing Then
+                    result = NotFound()
+                End If
+
+                If result Is Nothing Then
+                    mapper = New Mapper(m_mapperConfiguration)
+                    result = Ok(mapper.Map(Of Organization)(innerOrganization))
+                End If
+            End Using
+            Return result
+        End Function
 
         'todo add error handling
-        <HttpPut(), ClaimsAuthorization(ClaimTypes:="OA")> Public Function OrganizationGroup(<FromBody()> ByVal organization As Organization) As IHttpActionResult
+        <HttpPut(), ClaimsAuthorization(ClaimTypes:="OA")> Public Function SaveOrganization(<FromBody()> ByVal organization As Organization) As IHttpActionResult
             Dim result As IHttpActionResult = Nothing
             Dim innerOrganization As IOrganization = Nothing
             Dim organizationSaver As IOrganizationSaver
             Dim organizationFactory As IOrganizationFactory
             Dim mapper As IMapper
-            Using scope As ILifetimeScope = Me.ObjectContainer.BeginLifetimeScope
-                organizationFactory = scope.Resolve(Of IOrganizationFactory)()
-                If organization.OrganizationId.Equals(Guid.Empty) Then
-                    innerOrganization = organizationFactory.Create()
-                Else
-                    innerOrganization = organizationFactory.Get(New Settings(), organization.OrganizationId)
-                End If
 
-                If innerOrganization Is Nothing Then
-                    result = NotFound()
+            If organization Is Nothing Then
+                result = BadRequest("Missing organziation data")
+            End If
+
+            Using scope As ILifetimeScope = Me.ObjectContainer.BeginLifetimeScope
+                If result Is Nothing Then
+                    organizationFactory = scope.Resolve(Of IOrganizationFactory)()
+                    If organization.OrganizationId.Equals(Guid.Empty) Then
+                        innerOrganization = organizationFactory.Create()
+                    Else
+                        innerOrganization = organizationFactory.Get(New Settings(), organization.OrganizationId)
+                    End If
+
+                    If innerOrganization Is Nothing Then
+                        result = NotFound()
+                    End If
                 End If
 
                 If result Is Nothing Then
