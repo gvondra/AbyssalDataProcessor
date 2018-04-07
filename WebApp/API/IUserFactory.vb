056fc2130176fde4016e9e400456895392aee58d
Greg Vondra <gvondra@gmail.com>
2018-04-07 08:01:08 -0500

    Remove unused interface

--- WebApp/API/IUserFactory.vb
+++
@@ -1,6 +0,0 @@
-﻿Imports System.Security.Claims
-Public Interface IUserFactory
-    Inherits AbyssalDataProcessor.client.Framework.IUserFactory
-
-    Overloads Function [Get](ByVal principal As ClaimsPrincipal) As IUser
-End Interface
